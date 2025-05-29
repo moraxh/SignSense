fb17ef8bda9b56795ef28de46425a0b23a39f1d0
Jorge Mora <jorgemorah671@gmail.com>
2025-05-28 19:18:46 -0600

    fix: Correct ENTRYPOINT in Dockerfile and remove unused allowedHosts configuration in astro.config.mjs

--- app/frontend/astro.config.mjs
+++ app/frontend/astro.config.mjs
@@ -3,20 +3,10 @@ import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
-const allowedHostsEnv = process.env.ALLOWED_HOSTS || '';
-const allowedHosts = allowedHostsEnv
-  .split(',')
-  .map(host => host.trim())
-  .filter(Boolean);
-
 export default defineConfig({
   vite: {
     server: {
         watch: { usePolling: true },
-        allowedHosts: allowedHosts,
-    },
-    preview: {
-      allowedHosts: allowedHosts,
     },
     plugins: [tailwindcss()],
   },
